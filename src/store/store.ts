526439b8814f6e39805a9d2b99b1fd768b45744f
Agohige-Tech <60114461+Agohige-Tech@users.noreply.github.com>
2023-03-03 11:37:08 -0500

    feat(answers): Added in answers to provided questions

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,18 @@
-import { applyMiddleware, combineReducers, configureStore } from '@reduxjs/toolkit';
-import { appSlice } from './app/slice';
-import thunkMiddleware from 'redux-thunk'
+import {
+  applyMiddleware,
+  combineReducers,
+  configureStore,
+} from "@reduxjs/toolkit";
+import { appSlice } from "./app/slice";
+import thunkMiddleware from "redux-thunk";
 
 export const rootReducer = combineReducers({
   app: appSlice.reducer,
 });
 
 // QUESTION: Why would we use thunk middleware? What advantage does this give us?
-// ANSWER: 
-const composedEnhancer = applyMiddleware(thunkMiddleware)
+// ANSWER: Can dispatch multiple actions asyncronously. Helps with more complex things when utilizing Redux and expands our handling capabilities.
+const composedEnhancer = applyMiddleware(thunkMiddleware);
 
 export const store = configureStore({
   reducer: rootReducer,
